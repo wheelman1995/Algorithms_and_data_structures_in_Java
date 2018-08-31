1426e9ba2a47dc5d1729d51d56eb2a9127758c04
wheelman1995 <anikinvitya@gmail.com>
2018-08-31 22:53:27 +0300

    add lesson3

---
+++ lesson3/src/Stack.java
@@ -0,0 +1,46 @@
+package jads.three.online;
+
+import jads.two.home.Array;
+
+public class Stack {
+    private Array stack;
+    private int head;
+
+    public Stack(int size) {
+        this.stack = new Array(size);
+        this.head = -1;
+    }
+
+    public boolean isEmpty() {
+        return head == -1;
+    }
+
+    public void push(int value) {
+        //stack[++head] = value;
+        stack.append(value);
+        head++;
+    }
+
+    public int pop() {
+        //if (!isEmpty()) return stack[head--];
+        int val;
+        try {
+            val = stack.get(head--);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Stack is Empty");
+        }
+        stack.remove();
+        return val;
+    }
+
+    public int peek() {
+        //if (!isEmpty()) return stack[head];
+        int val;
+        try {
+            val = stack.get(head);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Stack is Empty");
+        }
+        return val;
+    }
+}
---
+++ lesson3/src/Third.java
@@ -0,0 +1,21 @@
+package jads.three.online;
+
+public class Third {
+    public static void main(String[] args) {
+        Stack s = new Stack(5);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        while (!s.isEmpty()) {
+            System.out.print("pop=" + s.pop() + " ");
+        }
+        System.out.println();
+        s.push(5);
+        s.push(6);
+        System.out.println("peek="+s.peek());
+
+    }
+}
