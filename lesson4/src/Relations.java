c6705cfc9db2429f75bfd5a8dcfc798bbb717e7f
wheelman1995 <anikinvitya@gmail.com>
2018-09-07 11:16:31 +0300

    add lesson4 homework

--- lesson4/src/Relations.java
+++ lesson4/src/Relations.java
@@ -5,20 +5,28 @@ import java.util.Set;
 
 public class Relations {
     public static void main(String[] args) {
-        RelatedList<Cat> rl = new RelatedList<>();
-        rl.insert(new Cat(1, "Barsik"));
-        rl.insert(new Cat(2, "Murzik"));
-        rl.insert(new Cat(4, "Kissik"));
-        System.out.println(rl);
-        System.out.println(rl.remove());
-        System.out.println(rl);
-        System.out.println(rl.contains(new Cat(2, "Murzik")));
+        DoubleLinkedList<Cat> dll = new DoubleLinkedList<>();
+        Cat c = new Cat(2, "Murzik");
+        dll.insert(new Cat(1, "Barsik"));
+        dll.insert(c);
+        dll.insert(new Cat(4, "Kissik"));
+//        System.out.println(dll);
+//        System.out.println(dll.delete(c));
+//        System.out.println(dll);
+//        System.out.println(dll.contains(new Cat(2, "Murzik")));
+        
 
-        HashMap<String, String> map = new HashMap<>();
-        Set<Map.Entry<String, String>> set = map.entrySet();
-        Iterator<Map.Entry<String, String>> iter = set.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next().getKey() + iter.next().getValue());
+//        HashMap<String, String> map = new HashMap<>();
+//        Set<Map.Entry<String, String>> set = map.entrySet();
+//        Iterator<Map.Entry<String, String>> iter = set.iterator();
+//        while (iter.hasNext()) {
+//            System.out.println(iter.next().getKey() + iter.next().getValue());
+//        }
+        
+        dll.insert(c);
+        
+        while (dll.hasNext()) {
+            System.out.println(dll.next());
         }
     }
 }
