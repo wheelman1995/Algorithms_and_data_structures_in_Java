46bbec0c6a7398792b1355d7ed83893afb4295f6
wheelman1995 <anikinvitya@gmail.com>
2018-09-08 10:47:33 +0300

    add lesson5

---
+++ lesson5/src/MyIterator.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+public interface MyIterator<T> extends Iterator {
+    void reset();
+    boolean atEnd();
+    boolean atBegin();
+    T deleteCurrent();
+    void insertAfter(T c);
+    void insertBefore(T c);
+    T getCurrent();
+}
